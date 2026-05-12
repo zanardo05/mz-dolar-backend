89080e2c909f84dfa35b5b6d2fc00d26300654db
zanardo05 <gabrielzanardo05@outlook.com>
2026-05-11 21:08:29 -0300

    Set up Express server with CORS and dotenv

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+
+  res.send("MZ Dólar Backend Online");
+
+});
+
+app.get("/auth/login", (req, res) => {
+
+  res.redirect(
+    "https://app.deriv.com"
+  );
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
+
+  console.log("Servidor online");
+
+});
