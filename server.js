162cb385cfde032046c9fdc6adc7b32d2d9ff0f1
zanardo05 <gabrielzanardo05@outlook.com>
2026-05-11 21:21:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import derivRoutes from "./routes/deriv.js";
+
 dotenv.config();
 
 const app = express();
@@ -9,22 +11,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", derivRoutes);
+
 app.get("/", (req, res) => {
 
-  res.send("MZ Dólar Backend Online");
-
-});
-
-app.get("/auth/login", (req, res) => {
-
-  res.redirect(
-    "https://app.deriv.com"
+  res.send(
+    "MZ Dólar Backend Online"
   );
 
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(
+  process.env.PORT || 3000,
+  () => {
 
-  console.log("Servidor online");
+    console.log(
+      "Servidor online"
+    );
 
-});
+  }
+);
