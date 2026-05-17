689c0bba95501984935adc20474a888db374743e
zanardo05 <gabrielzanardo05@outlook.com>
2026-05-16 21:01:52 -0300

    Update login route to use OAuth redirection
    Redirect to OAuth authorization URL instead of app.

--- routes/deriv.js
+++ routes/deriv.js
@@ -4,9 +4,18 @@ const router = express.Router();
 
 router.get("/login", async (req, res) => {
 
-  res.redirect(
-    "https://app.deriv.com"
-  );
+  const appId =
+    process.env.DERIV_APP_ID;
+
+  const redirect =
+    encodeURIComponent(
+      `${process.env.FRONTEND_URL}/#/dashboard`
+    );
+
+  const url =
+    `https://oauth.deriv.com/oauth2/authorize?app_id=${appId}&l=PT&redirect_uri=${redirect}`;
+
+  res.redirect(url);
 
 });
 
