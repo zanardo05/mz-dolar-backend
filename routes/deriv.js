16ff1fc65d7a9804498ebcaace241cc0fc410790
zanardo05 <gabrielzanardo05@outlook.com>
2026-05-16 21:18:38 -0300

    Update deriv.js

--- routes/deriv.js
+++ routes/deriv.js
@@ -9,13 +9,32 @@ router.get("/login", async (req, res) => {
 
   const redirect =
     encodeURIComponent(
-      `${process.env.FRONTEND_URL}/#/dashboard`
+      "https://mz-dolar-backend.onrender.com/auth/callback"
     );
 
   const url =
     `https://oauth.deriv.com/oauth2/authorize?app_id=${appId}&l=PT&redirect_uri=${redirect}`;
 
   res.redirect(url);
+
+});
+
+router.get("/callback", async (req, res) => {
+
+  const token =
+    req.query.token1;
+
+  if (!token) {
+
+    return res.send(
+      "Token não encontrado"
+    );
+
+  }
+
+  res.redirect(
+    `${process.env.FRONTEND_URL}/#/dashboard?token=${token}`
+  );
 
 });
 
